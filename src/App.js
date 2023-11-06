a8bc3cae442744f4e3d5a31838ae2fc99236ab16
angelogentileiii <140743863+angelogentileiii@users.noreply.github.com>
2023-11-06 08:41:02 -0500

    First upload to build basic framework of app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './';
+
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Page Header</h1>
+      </header>
+      <body>
+        <Home />
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import {useState, useEffect} from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+function Home() {
+    const [restaurantNews, setRestaurantNews] = useState([]);
+    
+    return (
+        <>
+            <p>Build The App</p>
+        </>
+    )
+}
+
+export default Home
